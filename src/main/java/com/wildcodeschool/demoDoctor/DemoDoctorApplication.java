32cc76db0bc381ea6d62ad3b20552f4f753ce5cb
AurelOconnell <aurelienoconnell@gmail.com>
2023-10-09 17:41:54 +0200

    1st spring quest done

---
+++ src/main/java/com/wildcodeschool/demoDoctor/DemoDoctorApplication.java
@@ -0,0 +1,13 @@
+package com.wildcodeschool.demoDoctor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoDoctorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoDoctorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wildcodeschool/demoDoctor/DemoDoctorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wildcodeschool.demoDoctor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoDoctorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
